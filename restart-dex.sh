b4d76f296331ac0e3acf98f61c956d7e64a73f2d
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-02-27 14:37:47 -0800

    Add restart-dex utility for when cluster is stopped/started

---
+++ restart-dex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Echo in OpenShift GitOps 1.5 you need to restart Dex if the cluster has been stopped and started.
+NUM_USERS=100
+
+for i in $(seq 1 $NUM_USERS);
+do
+  echo "Restart dex for user ${i}"
+  #oc rollout restart deploy argocd-dex-server -n "user${i}-argocd"
+done
